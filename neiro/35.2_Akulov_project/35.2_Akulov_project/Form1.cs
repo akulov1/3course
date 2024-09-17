cb4fe39dcc4996b72789f6e369be3405c82bab5c
Artyom <144591371+akulov1@users.noreply.github.com>
2024-09-17 11:58:54 +0300

    Add files via upload

---
+++ neiro/35.2_Akulov_project/35.2_Akulov_project/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _35._2_Akulov_project
+{
+    public partial class Form1 : Form
+    {
+        private double[] _inputPixels = new double[15] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Change_State(Button b, int index)
+        {
+            if (b.BackColor == Color.Black)
+            {
+                b.BackColor = Color.White;
+                _inputPixels[index] = 0;
+            }
+            else
+            if (b.BackColor == Color.White)
+            {
+                b.BackColor = Color.Black;
+                _inputPixels[index] = 1;
+            }
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Change_State(button1, 0);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Change_State(button2, 0);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Change_State(button3, 0);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Change_State(button4, 0);
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Change_State(button5, 0);
+
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Change_State(button6, 0);
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Change_State(button7, 0);
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Change_State(button8, 0);
+        }
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Change_State(button9, 0);
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Change_State(button10, 0);
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Change_State(button11, 0);
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Change_State(button12, 0);
+        }
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Change_State(button13, 0);
+        }
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Change_State(button14, 0);
+        }
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Change_State(button15, 0);
+        }
+    }
+}
