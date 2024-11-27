dd27e6e68a797dbae0e65728d71eee50b17b7fab
Artyom <144591371+akulov1@users.noreply.github.com>
2024-11-27 10:32:45 +0300

    Update import_xml.py

--- python/CGI/import_xml.py
+++ python/CGI/import_xml.py
@@ -1,27 +1,33 @@
 import sqlite3
-from xml.dom.minidom import parse
+from xml.dom import minidom
 
 def import_from_xml():
     db_path = 'cosmos.db'
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
-    doc = parse('planets.xml')
-
+    doc = minidom.parse('planets.xml')
     planets = doc.getElementsByTagName('Planet')
 
     for planet in planets:
-        name_element = planet.getElementsByTagName('Name')[0].firstChild.nodeValue
-        type_element = planet.getElementsByTagName('Type')[0].firstChild.nodeValue
-        distance_element = planet.getElementsByTagName('Distance')[0].firstChild.nodeValue
+        planet_id = int(planet.getElementsByTagName('ID')[0].childNodes[0].data)
+        name = planet.getElementsByTagName('Name')[0].childNodes[0].data
+        planet_type = planet.getElementsByTagName('Type')[0].childNodes[0].data
+        distance = float(planet.getElementsByTagName('Distance')[0].childNodes[0].data)
 
-        cursor.execute("INSERT INTO Planets (name, type, distance_from_sun) VALUES (?, ?, ?)",
-                       (name_element, type_element, distance_element))
+        # Проверка на существование записи
+        cursor.execute("SELECT COUNT(*) FROM Planets WHERE id = ?", (planet_id,))
+        count = cursor.fetchone()[0]
+
+        if count == 0:  # Если записи нет, вставляем новую
+            cursor.execute("INSERT INTO Planets (id, name, type, distance_from_sun) VALUES (?, ?, ?, ?)",
+                           (planet_id, name, planet_type, distance))
+        else:
+            print(f"Пропущена запись с ID {planet_id}, так как она уже существует.")
 
     conn.commit()
-
     conn.close()
-    print("Импорт завершен! Данные импортированы из planets.xml в базу данных.")
+    print("Импорт из XML завершён!")
 
 
 import_from_xml()
