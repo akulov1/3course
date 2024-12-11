8b9ca79688f859be0ac3b64b7c84f778490aece5
Artyom <144591371+akulov1@users.noreply.github.com>
2024-12-11 20:37:42 +0300

    Add files via upload

---
+++ neuro1/35.2_Akulov_project/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace _35._2_Akulov_project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.numericUpDownAnswer = new System.Windows.Forms.NumericUpDown();
+            this.buttonSaveTrainSample = new System.Windows.Forms.Button();
+            this.buttonSaveTestSample = new System.Windows.Forms.Button();
+            this.button_Recognize = new System.Windows.Forms.Button();
+            this.label_answer = new System.Windows.Forms.Label();
+            this.button_train = new System.Windows.Forms.Button();
+            this.button_test = new System.Windows.Forms.Button();
+            this.chart_Eavr = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label_probability = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAnswer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_Eavr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(16, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 45);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(67, 62);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 45);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(118, 62);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(45, 45);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(16, 113);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(45, 45);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(67, 113);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(45, 45);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(118, 113);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(45, 45);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(16, 164);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(45, 45);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(67, 164);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(45, 45);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(118, 164);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(45, 45);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.White;
+            this.button10.Location = new System.Drawing.Point(16, 215);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(45, 45);
+            this.button10.TabIndex = 9;
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.White;
+            this.button11.Location = new System.Drawing.Point(67, 215);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(45, 45);
+            this.button11.TabIndex = 10;
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.White;
+            this.button12.Location = new System.Drawing.Point(118, 215);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(45, 45);
+            this.button12.TabIndex = 11;
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.BackColor = System.Drawing.Color.White;
+            this.button13.Location = new System.Drawing.Point(16, 266);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(45, 45);
+            this.button13.TabIndex = 12;
+            this.button13.UseVisualStyleBackColor = false;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.BackColor = System.Drawing.Color.White;
+            this.button14.Location = new System.Drawing.Point(67, 266);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(45, 45);
+            this.button14.TabIndex = 13;
+            this.button14.UseVisualStyleBackColor = false;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button15
+            // 
+            this.button15.BackColor = System.Drawing.Color.White;
+            this.button15.Location = new System.Drawing.Point(118, 266);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(45, 45);
+            this.button15.TabIndex = 14;
+            this.button15.UseVisualStyleBackColor = false;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // numericUpDownAnswer
+            // 
+            this.numericUpDownAnswer.Location = new System.Drawing.Point(178, 215);
+            this.numericUpDownAnswer.Maximum = new decimal(new int[] {
+            9,
+            0,
+            0,
+            0});
+            this.numericUpDownAnswer.Name = "numericUpDownAnswer";
+            this.numericUpDownAnswer.Size = new System.Drawing.Size(59, 20);
+            this.numericUpDownAnswer.TabIndex = 15;
+            // 
+            // buttonSaveTrainSample
+            // 
+            this.buttonSaveTrainSample.Location = new System.Drawing.Point(169, 251);
+            this.buttonSaveTrainSample.Name = "buttonSaveTrainSample";
+            this.buttonSaveTrainSample.Size = new System.Drawing.Size(91, 25);
+            this.buttonSaveTrainSample.TabIndex = 16;
+            this.buttonSaveTrainSample.Text = "Сохранить обучающий пример";
+            this.buttonSaveTrainSample.UseVisualStyleBackColor = true;
+            this.buttonSaveTrainSample.Click += new System.EventHandler(this.buttonSaveTrainSample_Click);
+            // 
+            // buttonSaveTestSample
+            // 
+            this.buttonSaveTestSample.Location = new System.Drawing.Point(169, 283);
+            this.buttonSaveTestSample.Name = "buttonSaveTestSample";
+            this.buttonSaveTestSample.Size = new System.Drawing.Size(91, 28);
+            this.buttonSaveTestSample.TabIndex = 17;
+            this.buttonSaveTestSample.Text = "Сохранить тестовый пример";
+            this.buttonSaveTestSample.UseVisualStyleBackColor = true;
+            this.buttonSaveTestSample.Click += new System.EventHandler(this.buttonSaveTestSample_Click);
+            // 
+            // button_Recognize
+            // 
+            this.button_Recognize.Location = new System.Drawing.Point(169, 62);
+            this.button_Recognize.Name = "button_Recognize";
+            this.button_Recognize.Size = new System.Drawing.Size(91, 45);
+            this.button_Recognize.TabIndex = 18;
+            this.button_Recognize.Text = "Распознать";
+            this.button_Recognize.UseVisualStyleBackColor = true;
+            this.button_Recognize.Click += new System.EventHandler(this.button_Recognize_Click);
+            // 
+            // label_answer
+            // 
+            this.label_answer.AutoSize = true;
+            this.label_answer.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_answer.Location = new System.Drawing.Point(12, 9);
+            this.label_answer.Name = "label_answer";
+            this.label_answer.Size = new System.Drawing.Size(69, 37);
+            this.label_answer.TabIndex = 19;
+            this.label_answer.Text = "Out";
+            // 
+            // button_train
+            // 
+            this.button_train.Location = new System.Drawing.Point(169, 113);
+            this.button_train.Name = "button_train";
+            this.button_train.Size = new System.Drawing.Size(91, 26);
+            this.button_train.TabIndex = 20;
+            this.button_train.Text = "Обучить";
+            this.button_train.UseVisualStyleBackColor = true;
+            this.button_train.Click += new System.EventHandler(this.button_train_Click);
+            // 
+            // button_test
+            // 
+            this.button_test.Location = new System.Drawing.Point(169, 145);
+            this.button_test.Name = "button_test";
+            this.button_test.Size = new System.Drawing.Size(91, 35);
+            this.button_test.TabIndex = 21;
+            this.button_test.Text = "Тестировать";
+            this.button_test.UseVisualStyleBackColor = true;
+            this.button_test.Click += new System.EventHandler(this.button_test_Click);
+            // 
+            // chart_Eavr
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart_Eavr.ChartAreas.Add(chartArea1);
+            this.chart_Eavr.Location = new System.Drawing.Point(278, 9);
+            this.chart_Eavr.Name = "chart_Eavr";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            series1.Name = "Series_Eavr";
+            this.chart_Eavr.Series.Add(series1);
+            this.chart_Eavr.Size = new System.Drawing.Size(667, 300);
+            this.chart_Eavr.TabIndex = 22;
+            this.chart_Eavr.Text = "chart1";
+            title1.Name = "Title1";
+            title1.Text = "График средних энергий ошибок";
+            this.chart_Eavr.Titles.Add(title1);
+            // 
+            // label_probability
+            // 
+            this.label_probability.AutoSize = true;
+            this.label_probability.Location = new System.Drawing.Point(115, 27);
+            this.label_probability.Name = "label_probability";
+            this.label_probability.Size = new System.Drawing.Size(72, 13);
+            this.label_probability.TabIndex = 23;
+            this.label_probability.Text = "Вероятность";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 335);
+            this.Controls.Add(this.label_probability);
+            this.Controls.Add(this.chart_Eavr);
+            this.Controls.Add(this.button_test);
+            this.Controls.Add(this.button_train);
+            this.Controls.Add(this.label_answer);
+            this.Controls.Add(this.button_Recognize);
+            this.Controls.Add(this.buttonSaveTestSample);
+            this.Controls.Add(this.buttonSaveTrainSample);
+            this.Controls.Add(this.numericUpDownAnswer);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownAnswer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart_Eavr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.NumericUpDown numericUpDownAnswer;
+        private System.Windows.Forms.Button buttonSaveTrainSample;
+        private System.Windows.Forms.Button buttonSaveTestSample;
+        private System.Windows.Forms.Button button_Recognize;
+        private System.Windows.Forms.Label label_answer;
+        private System.Windows.Forms.Button button_train;
+        private System.Windows.Forms.Button button_test;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart_Eavr;
+        private System.Windows.Forms.Label label_probability;
+    }
+}
+
---
+++ neuro1/35.2_Akulov_project/NeuroNet/Network.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _35._2_Akulov_project.NeuroNet
+{
+    class Network
+    {
+        //sloi
+        private InputLayer input_layer = null;
+        private HiddenLayer hidden_layer1 = new HiddenLayer(70, 15, NeuronType.Hidden, nameof(hidden_layer1));
+        private HiddenLayer hidden_layer2 = new HiddenLayer(31, 70, NeuronType.Hidden, nameof(hidden_layer2));
+        private OutputLayer output_layer = new OutputLayer(10, 31, NeuronType.Output, nameof(output_layer));
+
+        public double[] fact = new double[10];
+
+        private double[] e_error_avr;//среднее значение энергии ошибки эпохи обучения
+
+        public double[] E_error_avr { get => e_error_avr; set => e_error_avr = value; } //срденне значение энергии эпохи обучения
+        
+        public Network() { }
+
+        public void Train(Network net)
+        {
+            int epoches = 100;
+            net.input_layer = new InputLayer(NetworkMode.Train);//инициализация вх слоя
+            double tmpSumError;
+            double[] errors;//массив сигнала ошибки
+            double[] temp_gsums1; //градиент первого слоя
+            double[] temp_gsums2;
+            double[] temp3;
+            e_error_avr = new double[epoches];
+            double[,] hiddenLayer1Weights = new double[70, 15];
+            double[,] hiddenLayer2Weights = new double[31, 70];
+            double[,] outputLayerWeights = new double[10, 31];
+
+            for (int k = 0; k < epoches; k++)//перебор эпох обучения
+            {
+                e_error_avr[k] = 0;//обновление энергии ошибки
+                for (int i = 0; i < net.input_layer.TrainSet.GetLength(0); i++)
+                {
+                    double[] tmpTrain = new double[15];
+                    for (int j = 0; j < tmpTrain.Length; j++)
+                        tmpTrain[j] = net.input_layer.TrainSet[i, j + 1];
+                    ForwardPass(net, tmpTrain);
+
+                    tmpSumError = 0;
+                    errors = new double[net.fact.Length];
+                    for (int x = 0; x < errors.Length; x++)
+                    {
+                        if (x == net.input_layer.TrainSet[i, 0])
+                            errors[x] = net.fact[x] - 1.0;
+                        else
+                            errors[x] = -net.fact[x];
+
+                        tmpSumError += errors[x] * errors[x] / 2;
+                    }
+                    e_error_avr[i] += tmpSumError / errors.Length;
+                    //обратный проход и коррекция весов
+                    temp_gsums2 = net.output_layer.BackwardPass(errors);
+                    temp_gsums1 = net.hidden_layer2.BackwardPass(temp_gsums2);
+                    temp3 = net.hidden_layer1.BackwardPass(temp_gsums1);
+
+                }
+                e_error_avr[k] /= net.input_layer.TrainSet.GetLength(0); // Средняя энергия ошибки k-ой эпохи
+            }
+            net.input_layer = null;
+
+            // Записываем веса в файлы
+            net.hidden_layer1.WeightInitialize(MemoryMode.SET,AppDomain.CurrentDomain.BaseDirectory + "memory\\hidden_layer1_memory.csv");
+            net.hidden_layer2.WeightInitialize(MemoryMode.SET,AppDomain.CurrentDomain.BaseDirectory + "memory\\hidden_layer2_memory.csv");
+            net.output_layer.WeightInitialize(MemoryMode.SET,AppDomain.CurrentDomain.BaseDirectory + "memory\\output_layer_memory.csv");
+        }
+            //прямой проход нейросети
+        public void ForwardPass(Network net, double[] netInput)
+        {
+            net.hidden_layer1.Data = netInput;
+            net.hidden_layer1.Recognize(null, net.hidden_layer2);
+            net.hidden_layer2.Recognize(null, net.output_layer);
+            net.output_layer.Recognize(net, null);
+        }
+
+
+
+    }
+}
